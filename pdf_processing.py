08d2b9f6fae82546ae49b63ebe321c39d609c99f
loadthecode0 <artyaastha@gmail.com>
2024-03-12 19:39:28 +0530

    included pymupdf and tika

--- pdf_processing.py
+++ pdf_processing.py
@@ -1,5 +1,8 @@
+import os
 import pypdfium2 as pdfium
 from pdfminer.high_level import extract_text
+import fitz
+from tika import parser 
 
 def write_output(text, dir, index):
     f = open("output_pdf_text/" + dir + "/pdf_text" + str(index) + ".txt", "w")
@@ -8,16 +11,12 @@ def write_output(text, dir, index):
 
 def pdfproc_pypdfium2 (index) :
     pdf = pdfium.PdfDocument("test_pdfs/test" + str(index) +".pdf")
-    version = pdf.get_version()  # get the PDF standard version
     n_pages = len(pdf)  # get the number of pages in the document
 
     text = ""
-    for i in range(0, n_pages):
-        
+    for i in range(0, n_pages):        
         page = pdf[i]  # load a page
-        # Load a text page helper
         textpage = page.get_textpage()
-        # Extract text from the whole page
         text += textpage.get_text_bounded()
 
     write_output(text, "pypdfium2", index)
@@ -26,9 +25,23 @@ def pdfproc_pdfminer (index):
     text = extract_text("test_pdfs/test" + str(index) +".pdf")
     write_output(text, "pdfminer", index)
     
-def pdfproc_pdfminer (index):
-    text = extract_text("test_pdfs/test" + str(index) +".pdf")
-    write_output(text, "pdfminer", index)
+def pdfproc_pymupdf (index):
+    pdf = fitz.open("test_pdfs/test" + str(index) +".pdf")
+    text = ""
+    for page in pdf:
+        text += page.get_text()
+    write_output(text, "pymupdf", index)
     
-pdfproc_pypdfium2(1)
-pdfproc_pdfminer(1)
+def pdfproc_tika (index):
+    pdf = parser.from_file("test_pdfs/test" + str(index) +".pdf")
+    text = pdf['content']
+    write_output(text, "tika", index)
+    
+path = "./test_pdfs"
+dir_list = os.listdir(path)
+
+for i in range(1, len(dir_list) + 1):
+    pdfproc_pypdfium2(i)
+    pdfproc_pdfminer(i)
+    pdfproc_pymupdf(i)
+    pdfproc_tika(i)
